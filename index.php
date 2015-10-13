a04ffe39ff231c26d58b1374d1e00cd1f60e238d
Shawn Minarik <minars@rpi.edu>
2015-10-13 16:17:07 -0400

    Initial Docs To Build Off
    Here are the initial docs we can build off of.

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<meta charset='utf-8'>
+<head>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+</body>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+</html>
