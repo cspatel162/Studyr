12f570e06ea446697654c3430c030951a67f7b6c
Sarah Abrahamson <abrahs3@rpi.edu>
2015-10-21 21:49:30 -0400

    Added splashpage templates and landing page for login
    The landing page should probably be the calendar after the user logs in,
    so I changed that in the login/index.php.

--- Login/index.php
+++ Login/index.php
@@ -4,7 +4,7 @@
     <head>
 
     <body>
-        <form action="process_form.php" method="post">
+        <form action="../php/calendar.php" method="post">
             Username: <input type="text" name="username"><br>
             Password: <input type="password" name="password">
             <input type="submit" value="submit">
---
+++ php/splashpage.php
@@ -0,0 +1,38 @@
+<?php?>
+<html>
+	<meta charset='utf-8'>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<link rel="stylesheet" type="text/css" href="../css/splashstyle.css">
+		<title>Studyr</title>
+	</head>
+	<body>
+		<section class="content">
+			<header class="rightheader"><a href="" id="signin">Sign In</a></header>
+			<img src="">
+			<section class="search">
+				<p>Search for Public Study Groups:</p>
+				<input type="text" class ="find">
+				<input type="submit" value="Search" class="find">
+			</section>
+			<section class="menu">
+				<p>Search Groups by Course Offerings:</p>
+				<ul class="nav">
+					<li><input type="button" value="ADMN" class="navitem"></li>
+					<li><input type="button" value="ARCH" class="navitem"></li>
+					<li><input type="button" value="ARTS" class="navitem"></li>
+					<li><input type="button" value="ASTR" class="navitem"></li>
+					<li><input type="button" value="BCBP" class="navitem"></li>
+				</ul>
+				<ul class="nav">
+					<li><input type="button" value="BIOL" class="navitem"></li>
+					<li><input type="button" value="BMED" class="navitem"></li>
+					<li><input type="button" value="CHEM" class="navitem"></li>
+					<li><input type="button" value="CHME" class="navitem"></li>
+					<li><input type="button" value="Show more..." class="navitem"></li>
+				</ul>
+			</section>
+		</section>
+	</body>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+</html>
