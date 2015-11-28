be5be8f6125a4e64d10450000428826a25bf5915
minars <minars@rpi.edu>
2015-11-28 14:15:50 -0500

    Search now pulls the events for each class that are not private.

--- php/search.php
+++ php/search.php
@@ -1,23 +1,29 @@
 <?php 
-require "connect.php";
 
-function search($prefix){
-	global $conn;
-	if($prefix == '*')
-		$sql = "SELECT class.*, professors.fname, professors.lname FROM class INNER JOIN professors ON class.professorID = professors.professorID";
-	else
-		$sql = "SELECT class.*, professors.fname, professors.lname FROM class INNER JOIN professors ON class.professorID = professors.professorID WHERE class.courseType = '$prefix'";
-	$results = $conn->query($sql);
-	printres($results);
-}
-if($_POST['submit']=='Show All...'){search('*');}
-else {search($_POST['submit']);}
+	$results;
+	require "connect.php";
+	function searchclasses($prefix){
+		global $conn;
+		if($prefix == '*')
+			$sql = "SELECT class.*, professors.fname, professors.lname,study_groups.* FROM class INNER JOIN professors ON class.professorID = professors.professorID INNER JOIN study_groups ON class.courseID = study_groups.courseID WHERE study_groups.privacy = 0";
+		else
+			$sql = "SELECT class.*, professors.fname, professors.lname,study_groups.* FROM class INNER JOIN professors ON class.professorID = professors.professorID INNER JOIN study_groups on class.courseID = study_groups.courseID WHERE class.courseType = '$prefix' and study_groups.privacy = 0";
+		global $results;
+		$results = $conn->query($sql);
+	}
+
+	if($_POST['submit']=='Show All...'){searchclasses('*');}
+	else {searchclasses($_POST['submit']);}
+
+	function printres($results){
+		foreach ($results as $val){
+			$meeting_time = date('Y-m-d H:i:s',strtotime($val['meetingTime']));
+			printf("<li id='result'>%u: %s %u, %s. Professor: %s %s <ul>",$val['crn'],$val['courseType'],$val['courseNumber'],$val['courseTitle'],$val['fname'],$val['lname']); 
+			printf("<li id='resultevent'>Group: %u, Meeting Time: %s </li>",$val['groupID'],$meeting_time);
+			printf("</ul></li>");
+		}
+	}
 ?>
-
-
-
-
-
 <html>
 <head>
 	<title> Search Results </title>
@@ -25,13 +31,9 @@ else {search($_POST['submit']);}
 <body>
 	<section id="results">
 		<ul id="resultlist">
-		<?php
-		function printres($results){
-			foreach ($results as $val){
-				printf("<li id='result'>%u: %s %u, %s. Professor: %s %s </li>",$val['crn'],$val['courseType'],$val['courseNumber'],$val['courseTitle'],$val['fname'],$val['lname']); 
-			}
-		}
-		?>
+			<?php
+				printres($results);	
+			?>
 	</ul>
 	</section>
 </body>
