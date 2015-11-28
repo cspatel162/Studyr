21418d16306114fb952b59154906d1e702d2aeba
minars <minars@rpi.edu>
2015-11-28 14:50:31 -0500

    Now shows classes even if there is no group thats not private

--- php/search.php
+++ php/search.php
@@ -5,9 +5,9 @@
 	function searchclasses($prefix){
 		global $conn;
 		if($prefix == '*')
-			$sql = "SELECT class.*, professors.fname, professors.lname,study_groups.* FROM class INNER JOIN professors ON class.professorID = professors.professorID INNER JOIN study_groups ON class.courseID = study_groups.courseID WHERE study_groups.privacy = 0";
+			$sql = "SELECT class.*, professors.fname, professors.lname,study_groups.* FROM class INNER JOIN professors ON class.professorID = professors.professorID INNER JOIN study_groups ON class.courseID = study_groups.courseID";
 		else
-			$sql = "SELECT class.*, professors.fname, professors.lname,study_groups.* FROM class INNER JOIN professors ON class.professorID = professors.professorID INNER JOIN study_groups on class.courseID = study_groups.courseID WHERE class.courseType = '$prefix' and study_groups.privacy = 0";
+			$sql = "SELECT class.*, professors.fname, professors.lname,study_groups.* FROM class INNER JOIN professors ON class.professorID = professors.professorID INNER JOIN study_groups on class.courseID = study_groups.courseID WHERE class.courseType = '$prefix'";
 		global $results;
 		$results = $conn->query($sql);
 	}
@@ -17,10 +17,15 @@
 
 	function printres($results){
 		foreach ($results as $val){
-			$meeting_time = date('Y-m-d H:i:s',strtotime($val['meetingTime']));
-			printf("<li id='result'>%u: %s %u, %s. Professor: %s %s <ul>",$val['crn'],$val['courseType'],$val['courseNumber'],$val['courseTitle'],$val['fname'],$val['lname']); 
-			printf("<li id='resultevent'>Group: %u, Meeting Time: %s </li>",$val['groupID'],$meeting_time);
-			printf("</ul></li>");
+			if($val['privacy'] == 0){
+				$meeting_time = date('Y-m-d H:i:s',strtotime($val['meetingTime']));
+				printf("<li id='result'>%u: %s %u, %s. Professor: %s %s <ul>",$val['crn'],$val['courseType'],$val['courseNumber'],$val['courseTitle'],$val['fname'],$val['lname']); 
+				printf("<li id='resultevent'>Group: %u, Meeting Time: %s </li>",$val['groupID'],$meeting_time);
+				printf("</ul></li>");
+			}
+			else{
+				printf("<li id='result'>%u: %s %u, %s. Professor: %s %s </li>",$val['crn'],$val['courseType'],$val['courseNumber'],$val['courseTitle'],$val['fname'],$val['lname']);
+			}
 		}
 	}
 ?>
