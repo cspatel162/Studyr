09d7febfda5c6f7b1e3d8abe592029a40c57775a
Sarah Abrahamson <abrahs3@rpi.edu>
2015-11-30 20:20:39 -0500

    Moved 'Logout' link to right

--- php/pageStart.php
+++ php/pageStart.php
@@ -34,7 +34,7 @@ TODO:
 	<section class="rightheader"><!-- top nav bar -->
 		<?php 
 			if(isset($_COOKIE['userID'])){ // Checks if the user is logged in and if so, supply them with some pages they can click other wise they can only go back to the main page.
-				echo '<a href="logout.php">Logout</a> | <a href="splashpage.php">Studyr</a> | <a href="calendar.php"> My Calendars</a>';
+				echo '<a href="calendar.php"> My Calendars</a> | <a href="splashpage.php">Studyr</a> | <a href="logout.php">Logout</a>';
 			}
 			else{
 				echo '<a href="splashpage.php">Studyr</a>';
