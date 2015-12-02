3f3d874246916d1ded1a60cbf3e705fd1cffa7dd
Sarah Abrahamson <abrahs3@rpi.edu>
2015-12-01 19:01:01 -0500

    Added time back into the current group listings

--- php/currentgroups.php
+++ php/currentgroups.php
@@ -11,20 +11,22 @@ TODO:
 	function fetchDate($datetime){
 		$pos = strrpos($datetime, " ");
 		$date = substr($datetime,0, $pos);
-		$time = substr($datetime,$pos+1);
+		$time = substr($datetime,$pos+1,5);
 		$datel =  explode("-",$date);
 		$months = array("00" => "notfound", "01" => "January", "02" => "February", "03" => "March",
 	   "04" => "April", "05" => "May", "06" => "June", "07" => "July", "08" => "August",
 	   "09" => "September", "10" => "October", "11" => "November", "12" => "December" );
-		return $months[$datel[1]] . " ". $datel[2] . ", " . $datel[0];
+		return $months[$datel[1]] . " ". $datel[2] . ", " . $datel[0] . " " .$time;
 	}
 	function currGroup(){
 		global $conn;
-		$sql = "SELECT events.*,locations.*,founderID FROM events INNER JOIN locations ON events.locationID = locations.locationID INNER JOIN study_groups ON events.groupID = study_groups.groupID WHERE events.userID = ".$_COOKIE['userID']; // gets events based on user id and displays more infromation about events
+		$id = $_COOKIE['userID'];
+		$sql = "SELECT events.*,locations.*,study_groups.founderID FROM events INNER JOIN locations ON events.locationID = locations.locationID 
+				INNER JOIN study_groups ON events.groupID = study_groups.groupID WHERE events.userID = ".$id ." ORDER BY events.startTime"; // gets events based on user id and displays more infromation about events
 		$results = $conn->query($sql);
 		if ($results->num_rows > 0){ // checks to make sure there are events, if 0 there are none and echos that out. 
 			foreach($results as $val){
-				if($val['founderID'] == $_COOKIE['userID']) {
+				if($val['founderID'] == $id) {
 					echo "<li class=\"currentgroup owngroup\"><p>".$val['eventName']."</p> Starting: ".fetchDate($val['startTime'])." At: ".$val['locationName']."</li>";
 				} else {
 					echo "<li class=\"currentgroup\"><p>".$val['eventName']."</p> Starting: ".fetchDate($val['startTime'])." At: ".$val['locationName']."</li>";
