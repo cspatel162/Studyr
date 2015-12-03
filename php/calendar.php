047fd3e04264710deb97a9e27a6d45820c9c384c
Steve Cardozo <cardos@rpi.edu>
2015-12-03 09:15:46 -0500

    Forgot to include this in last commit
    just moved everything to calendar.js file

--- php/calendar.php
+++ php/calendar.php
@@ -1,21 +1,6 @@
 <?php 
 	include_once "page_start.php";
 ?>
-				<script>
-					document.getElementById("calendar").innerHTML = '<button type="button" onclick="importCalendar()">Import Google Calendar</button>\
-					<table id="schedule">\
-						<tr>\
-							<td>Time</td> \
-							<td>Monday</td> \
-							<td>Tuesday</td> \
-							<td>Wednesday</td> \
-							<td>Thursday</td> \
-							<td>Friday</td> \
-							<td>Saturday</td> \
-							<td>Sunday</td> \
-						</tr>\
-					</table>';
-				</script>
 			</div>
 		</div>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
