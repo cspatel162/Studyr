24b3273b32e54e11d4f19e63801fb8be70a69b35
Steve Cardozo <cardos@rpi.edu>
2015-12-07 18:44:40 -0500

    Deleting unnecessary files

--- php/myschedule.php
+++
@@ -1,8 +0,0 @@
-<?php include_once "pageStart.php" ?>
-
-		<section id="schedule">
-		</section>
-	</section>
-</body>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-</html>
